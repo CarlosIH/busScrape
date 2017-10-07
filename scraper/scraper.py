1932f53fbf376f771d6c49c73bf81e0911cec7cc
Carlos Hernandez <carlos.israel.hernandez@gmail.com>
2017-10-07 12:27:18 -0700

    Improved scraper The scraper now checks for duplicate entries by checking a dictionary in memory instead of checking the database for every new entry.
    Scraper also loads the most recent entry for every bus from the database
    into memory to prevent duplicate entries upon restarting it. I also
    moved the place of the API key variable just to keep it close to the
    only other changing variable, the location of the database. Finally, the
    database file is now at the root of the repo.

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,39 +1,43 @@
 #This is a very bare scraper. In intervals on a single thread,
 #it dumps the currently running busses at AC transit into a database.
-#The routine to prevent duplicate entries is very inefficient but I have chosen
-#To focus my attention on the visual analysis of the busses.
 
 import sqlite3
 import urllib.request
 import time
 import json
 
-
-con = sqlite3.connect("./database.db")
-cur = con.cursor()
+con = sqlite3.connect("../database.db")
 apiKey = "api key here"
+
+cur = con.cursor()
 
 cur.execute("CREATE TABLE IF NOT EXISTS busList (id INTEGER PRIMARY KEY, "+
 	"BusId INTEGER, busLine VARCHAR(10), RecordedAtTime TEXT, "+
 	"latitude NUMERIC, longitude NUMERIC);")
 
-busListAddr = "http://api.511.org/transit/vehiclemonitoring?agency=actransit"+
-	"&api_key="+apiKey
+busListAddr = ("http://api.511.org/transit/vehiclemonitoring?agency=actransit"
+	"&api_key="+apiKey)
+
+#This dict stores the most recent TS of a bus
+busAge = {}
+
+#Initialize with the age of all busses currently in the database
+cur.execute("SELECT BusID, RecordedAtTime FROM busList GROUP BY busId ORDER BY RecordedAtTime ASC;")
+check = cur.fetchall()
+for bus in check:
+	busAge[bus[0]] = bus[1]
 
 while True:
 	busListRaw = urllib.request.urlopen(busListAddr).read().decode()[1:]
 	data = json.loads(busListRaw)
 	busList = data["Siri"]["ServiceDelivery"]["VehicleMonitoringDelivery"]["VehicleActivity"]
 	for bus in busList:
-		busInfo=bus["MonitoredVehicleJourney"]
+		busInfo = bus["MonitoredVehicleJourney"]
 
-		#check if entry already exists
-		cur.execute("SELECT id FROM busList WHERE busId == ? AND RecordedAtTime == ?",(busInfo["VehicleRef"],bus["RecordedAtTime"]))
-		check = cur.fetchone()
-
-		if check == None:
+		if busInfo["VehicleRef"] not in busAge or busAge[busInfo["VehicleRef"]] is not bus["RecordedAtTime"]:
 			cur.execute("insert into busList(busId, busLine, RecordedAtTime, latitude, longitude) VALUES (?, ?, ?, ?, ?)",
 				(busInfo["VehicleRef"], busInfo["LineRef"],bus["RecordedAtTime"],busInfo["VehicleLocation"]["Latitude"],busInfo["VehicleLocation"]["Longitude"]))
+			busAge[busInfo["VehicleRef"]] = bus["RecordedAtTime"]
 		else:
 			continue
 
